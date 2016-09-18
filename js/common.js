ccfb32f988a24fed77c9e118747bcd103f701576
Russlanpopular <rwwwalker@gmail.com>
2016-09-18 10:41:07 +0300

    update validate function

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(function() {
 
+
 	function Intersec(arr1,arr2){
 	 var idx = 0, arr3 = [];
 
@@ -15,21 +16,26 @@ $(function() {
 
 	function validate(text){
 
+		error = "";
+
 		var error_words = ['лес','wood','иерархия', 'маргарин', '<i>', '<section>'];
 
+		var Url = text.search( /(https?:\/\/)?(www\.)?([-а-яa-z0-9_\.]{2,}\.(рф|[a-z]{2,6}))((\/[-а-яa-z0-9_]{1,}\/)|(\/[-а-яa-z0-9_]{1,}\/)([-а-яa-z0-9_]{2,}\.(рф|[a-z]{2,6})))?((\?[a-z0-9_]{2,}=[-0-9]{1,})?((\&[a-z0-9_]{2,}=[-0-9]{1,}){1,})?)?/i);
+
 		text = text.split(" ");
+		
 
 		var words = Intersec(error_words, text);
 
-		for(var i = 0; i < text.length; i++){
-			var Url = text[i].search( /(https?:\/\/)?(www\.)?([-а-яa-z0-9_\.]{2,}\.(рф|[a-z]{2,6}))((\/[-а-яa-z0-9_]{1,}\/)|(\/[-а-яa-z0-9_]{1,}\/)([-а-яa-z0-9_]{2,}\.(рф|[a-z]{2,6})))?((\?[a-z0-9_]{2,}=[-0-9]{1,})?((\&[a-z0-9_]{2,}=[-0-9]{1,}){1,})?)?/i);
-		}
-
-		if(Url == 0){
+		if(Url != -1){
+			error = "Url";
 			return false;
 		}
 
 		if(words[0] != null){
+			for(var i = 0; i < words.length; i++){
+				error += " " + words[i];
+			}
 			return false;
 		}else{
 			return true;
@@ -38,13 +44,13 @@ $(function() {
 	}
 	
 	$("#title, #message").change(function(){
-
+			$(".write-post .error").remove();
 		if(validate($('#title').val().toLowerCase()) && validate($('#message').val().toLowerCase())){
 			$(".write-post .error").remove();
 			$(".write-post button").attr('disabled', false); 
 		}else{
 			if(!($("div").is(".error"))) {
-				$(".write-post").prepend("<div class='alert error alert-danger'><strong>Ошибка!</strong> Ведены неверные данные.</div>");
+				$(".write-post").prepend("<div class='alert error alert-danger'><strong>Ошибка!</strong> Ведены неверные данные "+error+" </div>");
 				$(".write-post button").attr('disabled', true); 
 			}
 		}
